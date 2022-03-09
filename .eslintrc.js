73f88c7511dd229d02abde3f0e7742b1f79312f3
Pieter Audiens <pieter.audiens@gmail.com>
2022-03-09 22:47:50 +0100

    Initial commit. I think I did not write any bugs... At least not by accident.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+      ],
+      extends: [
+
+      ],
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'error',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
+};
